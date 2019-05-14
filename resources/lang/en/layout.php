ade2d26ea3b7335d40fa85e5bf3bf668e6fde4df
Luclu7 <me@luclu7.fr>
2019-05-14 21:34:33 +0200

    english translations

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'js' => [
+        '_' => 'AnjaraFiles needs JavaScript to work properly.',
+        'tooltip' => 'AnjaraFiles will not work without JS. Please activate it, there\'s no tracking..'
+    ],
+
+    'menu' => [
+        'privacy' => 'Privacy',
+        'legal' => 'Terms'
+    ]
+
+];
---
+++ resources/lang/en/welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'title' => 'Welcome to AnjaraFiles !',
+    'synopsis' => 'AnjaraFiles is a file sharing system that is easy to use, does not require an account and which doesn\'t track you. Great, no?',
+
+    'groupname' => [
+        '_' => 'Name of the upload',
+        'placeholder' => 'My wonderful upload !'
+    ],
+
+    'expiry' => [
+        '_' => 'expiration date',
+        'placeholder' => 'Choose a period',
+        'day' => '1 day',
+        'week' => '1 week',
+        'month' => '1 month',
+        'year' => '1 year'
+    ],
+
+    'submit' => 'Submit!'
+
+];
