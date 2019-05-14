b1fa680ec4a14297ab9e1334680c71c2e359217d
Luclu7 <me@luclu7.fr>
2019-05-14 21:39:34 +0200

    AnjaraFile*S*

--- resources/lang/fr/layout.php
+++ resources/lang/fr/layout.php
@@ -3,8 +3,8 @@
 return [
 
     'js' => [
-        '_' => 'AnjaraFile nécessite le JavaScript pour fonctionner.',
-        'tooltip' => 'AnjaraFile ne fonctionnera pas sans le JS. Merci de l\'activer, il n\'y a pas d\'analytiques.'
+        '_' => 'AnjaraFiles nécessite le JavaScript pour fonctionner.',
+        'tooltip' => 'AnjaraFiles ne fonctionnera pas sans le JS. Merci de l\'activer, il n\'y a pas d\'analytiques.'
     ],
 
     'menu' => [
