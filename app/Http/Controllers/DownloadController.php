8056577a23f1a9402edd9c5caf8f271ee50b5e97
Leon <me@leonekmi.fr>
2019-05-16 12:08:10 +0200

    Update app/Http/Controllers/DownloadController.php

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -52,7 +52,7 @@ class DownloadController extends Controller
             } else {
                 if (Storage::exists('zips/'.$group->slug.'.zip')) {
                     // Storage::download('zips/'.$group->slug.'.zip', 'afilesdl.zip'); <- This doesn't work ? (the same works on L23 wtf)
-                    return response()->download(storage_path('app/zips/'.$group->slug.'.zip', ));
+                    return response()->download(storage_path('app/zips/'.$group->slug.'.zip', $group->name.'.zip'));
                 } else {
                     if (!Cache::get('job-group-'.$group->id, false)) {
                         ZipGroup::dispatch($group);
