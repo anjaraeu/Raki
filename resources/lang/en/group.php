d07cb577c5d76691dda5802b2e0bbba3f78ab8a6
Luclu7 <me@luclu7.fr>
2019-05-16 21:33:42 +0200

    add english translations

---
+++ resources/lang/en/group.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'welcome' => [
+        '_' => '":name" on AnjaraFiles',
+        'synopsis' => 'null'
+    ],
+
+    'download' => [
+        '_' => 'Download all',
+        'tooltip' => 'The generation of the Zip archive may take some time, the archive will be downloaded automatically when it is ready.'
+    ]
+
+];
