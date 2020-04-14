c583a9ddba84630c48110e960415b5d18b436135
Leon <me@leonekmi.fr>
2020-04-14 21:00:54 +0200

    whooops

---
+++ database/migrations/2020_04_11_221552_revamp_files_tables.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RevampFilesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->bigInteger('group_id')->unsigned()->nullable()->change();
+            $table->boolean('tus_uuid')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->bigInteger('group_id')->unsigned()->change();
+            $table->dropColumn('tus_uuid');
+        });
+    }
+}
